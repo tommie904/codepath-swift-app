f1f8021c849635d963a8342e09e8e1f25a9d760c
uzo <31833997+tommie904@users.noreply.github.com>
2025-08-24 19:54:03 -0500

    First commit

--- codepath-app/ViewController.swift
+++ codepath-app/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1);
+        let green = CGFloat.random(in: 0...1);
+        let blue = CGFloat.random(in: 0...1);
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0);
+    }
 
-
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        view.backgroundColor = changeColor();
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var schoolLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBAction func changeTextColor(_ sender: Any) {
+        let color = changeColor();
+        nameLabel.textColor = color;
+        schoolLabel.textColor = color;
+        jobLabel.textColor = color;
+    }
 }
 
